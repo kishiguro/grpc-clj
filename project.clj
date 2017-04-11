7dbbf36fbbd7271ae77ed4a166c00e0d991a894d
Kunihiro Ishiguro <kunihiroishiguro@gmail.com>
2017-04-10 21:19:04 -0700

    Update grpc package version to 1.2.0.

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.grpc/grpc-core "1.1.2"]
-                 [io.grpc/grpc-netty "1.1.2" :exclusions [io.grpc/grpc-core]]
-                 [io.grpc/grpc-protobuf "1.1.2"]
-                 [io.grpc/grpc-stub "1.1.2"]
+                 [io.grpc/grpc-core "1.2.0"]
+                 [io.grpc/grpc-netty "1.2.0" :exclusions [io.grpc/grpc-core]]
+                 [io.grpc/grpc-protobuf "1.2.0"]
+                 [io.grpc/grpc-stub "1.2.0"]
                  [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot grpc-clj.core
   :aot [grpc-clj.server]
